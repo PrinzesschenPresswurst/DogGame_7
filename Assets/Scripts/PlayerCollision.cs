15765368f74c41a365a2fb4d1e0a55862f80daf2
PrinzesschenPresswurst <prinzesschenpresswurst@googlemail.com>
2024-01-04 20:45:02 +0200

    made build

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.Android;
+
 using UnityEngine.Playables;
 using UnityEngine.Rendering.HighDefinition;
 
